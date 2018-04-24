8d5db8a2a1ed159ffd1f2cceaa0bb93827d93291
Paulina Salem <paulinasalem@yahoo.com>
2018-04-24 23:20:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,6 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize}
 
-end
 puts "#{planeteer_calls}!"
 end
 def long_planeteer_calls# code an argument here
